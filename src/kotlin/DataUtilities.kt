2b905124cd19bb1034991695c7e945b44cf93246
DavidLakeT <djcardonan@eafit.edu.co>
2023-04-13 01:46:03 -0500

    Updated structure and added some TO-DO

---
+++ src/kotlin/DataUtilities.kt
@@ -0,0 +1,10 @@
+package kotlin
+
+import java.io.InputStream
+import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
+
+fun readDataset() {
+    // TO-DO:
+    // - Implement reading for .csv files (and mapping for Student object)
+    // - Before doing this student object fields must have been defined
+}
---
+++ src/kotlin/Student.kt
@@ -0,0 +1,7 @@
+package kotlin
+
+data class Student (
+    // TO-DO:
+    // - Implement fields for Student class
+    // - Those fields must be selected from selection technique (eg. Gini)
+)
