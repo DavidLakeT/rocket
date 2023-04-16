dda9e11ac3118de7af19b69ee824c94424535fd0
David Cardona N <djcardonan@eafit.edu.co>
2023-04-15 20:09:07 -0500

    Column read by List

--- app/src/main/kotlin/rocket/DataProcessing.kt
+++ app/src/main/kotlin/rocket/DataProcessing.kt
@@ -4,12 +4,13 @@ import org.jetbrains.kotlinx.dataframe.DataFrame
 import org.jetbrains.kotlinx.dataframe.api.*
 import org.jetbrains.kotlinx.dataframe.io.read
 import java.io.File
+import org.jetbrains.kotlinx.dataframe.columns.toColumnSet
 
 class DataProcessing {
     fun readDataframe() {
 
-        val df = DataFrame.read(
-            "src/main/resources/grandecito.csv",
+        var df = DataFrame.read(
+            "src/main/resources/lite.csv",
             delimiter = ';',
         )
 
@@ -25,6 +26,32 @@ class DataProcessing {
 
          */
 
-        println(df["estu_genero.1", "exito"])
+        // "fami_numlibros", "estu_depto_reside.1", "estu_genero.1",
+
+        /*
+        // eliminar las columnas que no son numéricas o que tienen valores faltantes
+        val numericCols = df.numericColumns().names().filterNot { col -> df.column(col).isMissing.any() }
+
+        // calcular el coeficiente de correlación entre cada columna numérica y la columna "exito"
+        val correlations = mutableMapOf<String, Double>()
+        for (col in numericCols) {
+            val x = df.column(col).toDoubleArray()
+            val y = df.column("exito").toDoubleArray()
+            val model = OLS.fit(x, y)
+            correlations[col] = model.correlation
+        }
+
+        // mostrar las correlaciones ordenadas de mayor a menor
+        correlations.toList().sortedByDescending { (_, value) -> abs(value) }.forEach { (col, corr) ->
+            println("$col: $corr")
+        }
+
+        */
+
+        val columns = listOf("punt_filosofia", "punt_biologia", "exito").toColumnSet()
+
+        val test = df.select { columns }
+
+        print(test)
     }
 }
