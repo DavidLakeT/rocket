9a26cc84fdfab7409aaa8d769bc5dda7de47e5e2
jdvalencit <jdvalencit@eafit.edu.co>
2023-04-18 01:37:03 -0500

    Added case-handling for user input

--- app/src/main/kotlin/rocket/App.kt
+++ app/src/main/kotlin/rocket/App.kt
@@ -9,5 +9,21 @@ import com.github.doyaaaaaken.kotlincsv.client.CsvReader
 fun main() {
     val processer = DataProcessing()
     
-    processer.processC45()
+    println("Choose one of the following options:")
+    println("1. C45")
+    println("2. Random Forest")
+    println("3. Both")
+
+    val input = readLine()
+    print("Enter a number: ")
+
+    when (input) {
+        "1" -> processer.processC45()
+        "2" -> processer.processRandomForest()
+        "3" -> {
+            processer.processC45()
+            processer.processRandomForest()
+        }
+        else -> println("Not an option")
+    }
 }
