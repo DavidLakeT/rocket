76ad8414bd2ac2104740c06d95cbb31a906571ea
jdvalencit <jdvalencit@eafit.edu.co>
2023-04-18 10:35:00 -0500

    Added options loop and new exit option

--- app/src/main/kotlin/rocket/App.kt
+++ app/src/main/kotlin/rocket/App.kt
@@ -5,25 +5,29 @@ import java.util.Scanner
 import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
 import com.github.doyaaaaaken.kotlincsv.dsl.context.InsufficientFieldsRowBehaviour
 import com.github.doyaaaaaken.kotlincsv.client.CsvReader
+import kotlin.io.println
 
 fun main() {
     val processer = DataProcessing()
     
-    println("Choose one of the following options:")
-    println("1. C45")
-    println("2. Random Forest")
-    println("3. Both")
+    do{
+        println("Choose one of the following options:")
+        println("1. C45")
+        println("2. Random Forest")
+        println("3. Both")
+        println("4. Exit")
 
-    val input = readLine()
-    print("Enter a number: ")
+        val input = readLine()
 
-    when (input) {
-        "1" -> processer.processC45()
-        "2" -> processer.processRandomForest()
-        "3" -> {
-            processer.processC45()
-            processer.processRandomForest()
+        when (input) {
+            "1" -> processer.processC45()
+            "2" -> processer.processRandomForest()
+            "3" -> {
+                processer.processC45()
+                processer.processRandomForest()
+            }
+            "4" -> println("Exiting")
+            else -> println("Not an option")
         }
-        else -> println("Not an option")
-    }
+    } while (input != "4")
 }
